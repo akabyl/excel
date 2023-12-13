d3be31e59a3cd6ee3fb3e62216e85a4185c19074
akabyl <abkabylbekov97@gmail.com>
2023-12-13 15:58:08 +0500

    Finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,23 @@
 // eslint-disable-next-line quotes
 import {ExcelComponents} from "../../core/ExcelComponents";
 import {createTable} from './table.template';
+import {resizeHandler} from "./table.resize";
+import { shouldResize } from "./table.functions";
 
 export class Table extends ExcelComponents {
   static className = 'excel__table'
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
+
   toHTML() {
     return createTable(21)
+  }
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event)
+    }
   }
 }
